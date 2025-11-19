4aed2bb2765ae9589c10d5bb4458ccba1ede9d61
Ronald <ronaldrene91@gmail.com>
2025-11-18 23:43:06 -0300

    Added key pressed on the x axis and the frame rate to 60 fps

--- src/main.py
+++ src/main.py
@@ -4,18 +4,34 @@ import constans
 
 pygame.init()
 
+#Set the title for the window
+pygame.display.set_caption("Mi Primer Juego")
 #Create the window for the game
 screen = pygame.display.set_mode((constans.WINDOW_WIDTH,constans.WINDOW_HEIGHT))
 
+clock = pygame.time.Clock()
+
+xpos = 50
+
 #The Game Loop
 while True:
+    clock.tick(constans.FPS)
     #Polling the events generated from the user
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
+    pressed_keys = pygame.key.get_pressed()
+    if pressed_keys[K_RIGHT]:
+        xpos += 1
+    if pressed_keys[K_LEFT]:
+        xpos -= 1
+
+
     screen.fill((200,200,200))
     #Draw some shapes to the screen
-    pygame.draw.circle(screen,(0,180,200), (150,150), 10, 2) #(target_screen, circle_color, circle_position, size, border)
+    pygame.draw.circle(screen,(0,180,200), (xpos,150), 10, 2) #(target_screen, circle_color, circle_position, size, border)
+    pygame.draw.rect(screen,(100,40,85),(150,100,100, 15), 1) #(target_screen, rectangle_color, (rectangle_position, width, height), border)
     #Show the images in the screen and update the frame
     pygame.display.update()
+    
