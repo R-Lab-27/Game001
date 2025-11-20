87039a630961ae004100b061796c4c3eca24cf61
Ronald <ronaldrene91@gmail.com>
2025-11-20 00:24:03 -0300

    Added a platform class to be able to track objects with collitions

---
+++ src/plataformas.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.image.fill((100, 200, 100))
+        self.rect = self.image.get_rect(topleft=(x, y))
